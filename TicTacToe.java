14a3d164ff800c9c80b0377c856b3383dffeed5f
MrAtomicBombHead <142016095+MrAtomicBombHead@users.noreply.github.com>
2024-09-10 20:15:17 -0500

    Create TicTacToe.java

---
+++ TicTacToe.java
@@ -0,0 +1,16 @@
+public class TicTacToe {
+  int[] board = new int[9];
+  /** board space ids
+  0 | 1 | 2
+  3 | 4 | 5
+  6 | 7 | 8
+  Set the space to 0 for empty, 1 for X, 2 for O */
+  
+  public static void main(String args) {
+    //run game loop here
+  }
+
+  private static boolean checkWin() {
+    //check the board and return true if game is over.
+  }
+}
